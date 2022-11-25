6daa9857b8b84a6ff526503f1a56a63a4ba4d4b6
cemalsenel <senelcemal61@gmail.com>
2022-11-25 07:56:54 +0100

    add if-else statements

---
+++ ifStatements.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+        var a, b string = "kolkata", "Kolkata"
+        if a == b {
+                fmt.Println("strings are equal")
+        } else {
+                fmt.Println("strings are not equal")
+        }
+        fmt.Println("thank you!")
+
+}
