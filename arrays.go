2e5e7c1be319ae149ec6eeaf6b7f301d5ecdba3b
cemalsenel <senelcemal61@gmail.com>
2022-11-27 11:58:01 +0100

    add arrays

--- arrays.go
+++ arrays.go
@@ -3,12 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
+	var grades_1 [3]int = [3]int{1, 2, 3}
+	grades_2 := [3]int{1, 2, 3}
+	grades_3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(grades_1)
+	fmt.Println(grades_2)
+	fmt.Println(grades_3)
 
-	primes := [6]int{2, 3, 5, 7, 11, 13}
-	fmt.Println(primes)
+
+	var fruits [2]string = [2]string{"apple", "orange"}
+	fmt.Println(len(fruits))
+	fruits[0] = "kiwi"
+	fmt.Println(fruits[0])
+
+	for i := 0; i < len(fruits); i++{
+		fmt.Println(fruits[i])
+	}
+
+	for index, element := range fruits{
+		fmt.Println(index, "=>", element)
+	}
+
+	var multi [4][3]int = [4][3]int{{1,2,3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}
+
+	fmt.Println(multi[0][0])
+
+	for i := 0; i < len(multi); i++{
+		fmt.Println(multi[i])
+	}
+
+	arr1 := [5]bool{true, true, true, true}
+        for i := 0; i < len(arr1); i++ {
+                if arr1[i] {
+                        fmt.Println(i)
+                }
+        }
+	
+		arr := [5]string{"a", "b", "c"}
+        for index, element := range arr {
+                fmt.Println(index, "->", element)
+        }
 }
